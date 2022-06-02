b489aea56154f66f7a71f5cd4ba89e26fb6f0a74
abdourakhman <abdourakhman920@gmail.com>
2022-06-02 06:10:59 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+* main -Entry point
+* display a message
+* Return: Always 0(Success)
+*/
+int main(void)
+{
+	fprintf(stderr, "with proper grammar, but the outcome is a piece of art,\n");
+	return (1);
+}
