b25561b30cb774d9abdf433975df819781a73d4f
abdourakhman <abdourakhman920@gmail.com>
2022-06-03 07:53:14 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+
+
+/**
+* main - Entry point
+* show alphabet
+* Return: Always 0 (SUCCESS)
+*/
+
+int main(void)
+{
+	char letter;
+	char end = 'z'
+
+	for (letter = 'a'; letter <= end; letter++)
+	{
+		putchar(letter);
+		if (letter == end)
+		{
+			letter = 'A';
+			end = 'Z';
+		}
+	}
+	putchar('\n');
+	return (0);
+}
