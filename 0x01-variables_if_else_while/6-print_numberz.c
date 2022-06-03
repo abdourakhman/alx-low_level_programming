2eb4e2d02680ce62cc5cb629b07759b69995502d
abdourakhman <abdourakhman920@gmail.com>
2022-06-03 09:10:46 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* show alphabet
+* Return: Always 0 (SUCCESS)
+*/
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+
+}
