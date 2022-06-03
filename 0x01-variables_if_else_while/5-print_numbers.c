b1a0203086e5b569630aeaf12262b0e6988646a8
abdourakhman <abdourakhman920@gmail.com>
2022-06-03 08:45:54 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+
+/**
+* main - Entry point
+* show alphabet
+* Return: Always 0 (SUCCESS)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i=0; i < 10 ; i++)
+		puts(i);
+	putchar('\n');
+	return (0);
+}
