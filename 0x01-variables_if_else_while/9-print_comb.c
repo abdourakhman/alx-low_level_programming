9c998c589653cecd7e581c6378883082cbb388cd
abdourakhman <abdourakhman920@gmail.com>
2022-06-03 10:17:27 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,7 @@ int main(void)
 	{
 		putchar('0' + c);
 		if (c == 9)
-<<<<<<< HEAD
 			break;
-=======
-			continue;
->>>>>>> 8bddefb76109b9faea3179799de66c3d137bb506
 		putchar(',');
 		putchar(' ');
 	}
