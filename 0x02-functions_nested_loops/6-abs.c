9b2024bb21aa05c84ef47e5c7530120a02d80048
abdourakhman <abdourakhman920@gmail.com>
2022-06-07 09:09:35 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _abs - give the absolute value of an integer.
+* @n: The intger checked
+* Return: n positif always
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n*-1);
+	_putchar('\n');
+}
