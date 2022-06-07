d4ffa1d4800c284974fbd653644811fc83bb8f4e
abdourakhman <105052688+abdourakhman@users.noreply.github.com>
2022-06-07 16:05:44 +0200

    0-putchar.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar as a message.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int message[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i;
+	int size;
+
+	sz = sizeof(message) / sizeof(int);
+	for (i = 0; i < size; i++)
+	{
+		_putchar(message[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
