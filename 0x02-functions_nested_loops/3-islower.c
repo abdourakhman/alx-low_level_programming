162b4b20f57dba9f8b1abc7bceb41bebc45c28f8
abdourakhman <abdourakhman920@gmail.com>
2022-06-07 08:36:09 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*  is_lower - check if the character is miniscule
+*
+* Return: Always 1 if true
+* 0 otherwise
+*/
+int is_lower(int c)
+{
+	if(c >= 97 && c <=122
+		return 1;
+	else
+		return 0;
+	putchar('\n');
+}
