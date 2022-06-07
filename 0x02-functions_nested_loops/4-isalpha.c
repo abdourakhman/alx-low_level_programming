c8e425f93014d0dfe46039937ec103abcbfded54
abdourakhman <abdourakhman920@gmail.com>
2022-06-07 08:46:31 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-* _islower - check if the character is miniscule
+* _isalpha - check if the character is miniscule
 * @c: The character in ASCII code
 * Return: Always 1 if true
 * 0 otherwise
 */
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
 	else
 		return (0);
