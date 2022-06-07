b5d0a0914dae68f1cc99ac87eb86db9cf0f42382
abdourakhman <abdourakhman920@gmail.com>
2022-06-07 08:23:10 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+*  print_alphabet_x10 - display alphabet from a to z 10 times
+*
+* Return: Always 0 (SUCCESS)
+*/
+void print_alphabet_x10(void)
+{
+	char letter;
+<<<<<<< HEAD
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
+=======
+  int i;
+
+  for (i = 0; i < 10; i++)
+  {
+	  for (letter = 'a'; letter <= 'z'; letter++)
+		  _putchar(letter);
+	  _putchar('\n');
+  }
+>>>>>>> 1838a19f0be4d42992622a82b2df7c81345aedd7
+}
