e6bc95d986a777fa7d2c72782e1fc633188a29ed
abdourakhman <105052688+abdourakhman@users.noreply.github.com>
2022-06-09 14:46:07 +0200

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,16 +12,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	printf("%d",1);
+	for (i = 2; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("fizzbuzz ");
+			printf(" fizzbuzz");
 		else if (i % 3 == 0)
-			printf("fizz ");
+			printf(" fizz");
 		else if (i % 5 == 0)
-			printf("buzz ");
+			printf("buzz");
 		else
-			printf("%d ", i);
+			printf(" %d", i);
 	}
 	printf("\n");
 	return (0);
