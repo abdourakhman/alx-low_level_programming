e26681831aa48547d63285ba7e93996cde9ae760
abdourakhman <abdourakhman920@gmail.com>
2022-06-09 00:58:14 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+* isupper - check if the character is in uppercase
+* @c integer that stand for the ASCII code of the character
+* Return: 1 if true and 0 otherwise
+*/
+int isupper(int c)
+{
+	if (c >= 65 && c <=90)
+		return (1);
+	if (c >=97 && c <= 122)
+		return (0);
+
+}
+ 
