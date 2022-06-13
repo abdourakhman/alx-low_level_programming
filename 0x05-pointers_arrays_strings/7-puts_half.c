6be993d14264ac7d27e523fa93cc818d8de2f556
abdourakhman <abdourakhman920@gmail.com>
2022-06-13 11:32:13 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -24,8 +24,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		for (i = (length - 1) / 2; i < length -1; i++)
+		for (i = (length - 1) / 2; i < length; i++)
 			_putchar(str[i]);
 	}
-	_putchar('\n');
 }
