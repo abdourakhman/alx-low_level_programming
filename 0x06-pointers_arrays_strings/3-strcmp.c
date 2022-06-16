81644cee7b334838e54c2934bc720796d506383f
abdourakhman <abdourakhman920@gmail.com>
2022-06-15 17:53:53 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,38 +1,43 @@
-#include "main.h"
-
 /**
- * _strcmp - compare two strings
- * @s1: string1
- * @s2: string2
- * Return: 0->matching 15->s1>s2 and -15->s2>s1
- */
-int _strcmp(char *s1, char *s2)
-{
-	int i = 0;
-	int sign;
 
-	while ((s1[i] != '\0' || s2[i] != '\0'))
+ * _strcmp - compares two strings
+
+ * @s1: first string.
+
+ * @s2: second string.
+
+ * Return: 0 if s1 and s2 are equals,
+
+ * another number if not.
+
+ */
+
+
+
+int _strcmp(char *s1, char *s2)
+
+{
+
+	int i = 0, op = 0;
+
+
+
+	while (op == 0)
+
 	{
-		if (s1[i] == s2[i])
-			continue;
-		else if (s1[i] > s2[i])
-		{
-			sign = 1;
+
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+
 			break;
-		}
-		else
-		{
-			sign = -1;
-			break;
-		}
+
+		op = *(s1 + i) - *(s2 + i);
+
 		i++;
+
 	}
-	i = 0;
-	while (s1[i] != '\0' || s2[i] != '\0')
-	{
-		if ((s1[i] != s2[i]) || (s2[i] != s1[i]))
-			return (15  * sign);
-		i++;
-	}
-	return (0);
+
+
+
+	return (op);
+
 }
