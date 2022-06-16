1dbbe87622952879baacd8fa58eb8a2c864a480a
abdourakhman <abdourakhman920@gmail.com>
2022-06-15 17:04:02 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,34 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
+	int sign;
 
-	while (i > 0)
+	while (i >= 0)
 	{
-		if (s1[i] != s2[i])
-			return (-1);
+		if (s1[i] == '\0' || s2[i] == '\0')
+			break;
+		if (s1[i] == s2[i])
+			continue;
+		else if (s1[i] > s2[i])
+		{
+			sign = 1;
+			break;
+		}
+		else
+		{
+			sign = -1;
+			break;
+		}
+		i++;
 	}
-	return (1);
+	i = 0;
+	while (i >= 0)
+	{
+		if (s1[i] == '\0' || s2[i] == '\0')
+			break;
+		if ((s1[i] != s2[i]) || (s2[i] != s1[i]))
+			return (15  * sign);
+		i++;
+	}
+	return (0);
 }
