1dc364ccc00cc31b600055e570c2098d5376f907
abdourakhman <abdourakhman920@gmail.com>
2022-06-24 16:39:13 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,7 @@
 * @argv: a array that containts arguments passed
 * Return: 0 always success
 */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]__attribute__((unused)))
 {
 	if (argc > 0)
 		printf("%d\n", argc);
